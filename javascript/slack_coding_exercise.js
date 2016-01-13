80c989b9c12df79f29c63272a6aed0b62879b6a5
Amber <music4615@gmail.com>
2016-01-13 15:14:35 -0800

    tags summary

--- javascript/slack_coding_exercise.js
+++ javascript/slack_coding_exercise.js
@@ -1,13 +1,46 @@
+jQuery.fn.tagName() = function() {
+    return this.prop("tagName").toLowerCase();
+}
+
+function CountTag(dom, tagDictionary) {
+    if (dom.childNodes.length !== 0) {
+        for (var i = 0; i < dom.childNodes.length; i++) {
+            CountTag(dom.childNodes[i], tagDictionary);
+        }
+    }
+    var tag = $(dom).tagName;
+    if (tag !== undefined) {
+        if (!(tag in tagDictionary)) {
+            tagDictionary[tag] = 1;
+        } else {
+            tagDictionary[tag] += 1;
+        }
+    }
+}
+
 function TagSummary(htmlText) {
-    document.getElementById("tagSummary").innerHTML = "<h2>HTML Tags Summary</h2>";
+    var header = $("<h2></h2>").text("HTML Tags Summary");
+    $("#tagSummary").empty();
+    $("#tagSummary").append(header);
     var parser = new DOMParser();
     var htmlDoc = parser.parseFromString(htmlText, "text/html");
-    console.log(htmlDoc.firstChild);
+    var tagDic = new Array();
+    CountTag(htmlDoc, tagDic);
+
+    var ul = $("<ul id='tagList'></ul>");
+    for (var tag in tagDic) {
+        var li = $("<li></li>");
+        var ele = $("<span class='tag' id='" + tag + "'></span>");
+        $(li).append(ele);
+        $(li).append(": " + tagDic[tag]);
+        $(ul).append(li);
+    }
 }
 
 function GenPage(data) {
     console.log("Success");
-    var dataTransform = data.replace(/&/gm, "&amp;").replace(/</gm, "&lt;").replace(/>/gm, "&gt;");
+    var dataTransform = data.replace(/&/gm, "&amp;").replace(/</gm, "&lt;")
+                            .replace(/>/gm, "&gt;");
     var str = "<h2>HTML Document</h2><pre>" + dataTransform + "</pre>";
     document.getElementById("htmlSource").innerHTML = (str);
     TagSummary(data);
